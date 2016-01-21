d7f26419db9adcf9151f54e3058b594ec927ef04
xiaodoli <xiaodoli@microsoft.com>
2016-01-20 19:59:31 -0800

    unit test for core

---
+++ Telemetry/Telemetry.Core.UnitTests/ConfigTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Telemetry.Core.UnitTests
+{
+    [TestClass]
+    public class ConfigTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
