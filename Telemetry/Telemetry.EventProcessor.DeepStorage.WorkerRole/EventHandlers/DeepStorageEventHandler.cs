290a4625e87e34e914bb988e3ce681453710aa51
xiaodoli <xiaodoli@microsoft.com>
2016-01-27 15:35:06 -0800

    deep storae worker

---
+++ Telemetry/Telemetry.EventProcessor.DeepStorage.WorkerRole/EventHandlers/DeepStorageEventHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telemetry.EventProcessor.DeepStorage.Worker.EventHandlers
+{
+    using System.Collections.Concurrent;
+    using System.Timers;
+    using Microsoft.Practices.Unity;
+    using Microsoft.ServiceBus.Messaging;
+    using NLog;
+    using Telemetry.Core;
+    using Telemetry.Core.EventProcessor.Spec;
+    using Telemetry.EventProcessor.DeepStorage.Worker.EventStores;
+
+    public class DeepStorageEventHandler : IEventHandler
+    {
+        private readonly Logger _log;
+        private static ConcurrentDictionary<string, IEventStore> _eventStores = new ConcurrentDictionary<string, IEventStore>();
+        private static Timer _storeFlushTimer;
+
+        static DeepStorageEventHandler()
+        {
+            var overdueStoreFlushTime = Config.Parse<TimeSpan>("EventStores.OverdueFlushPeriod");
+            _storeFlushTimer = new Timer(overdueStoreFlushTime.TotalMilliseconds);
+            _storeFlushTimer.Elapsed += FlushOverdueStores;
+            _storeFlushTimer.Start();
+        }
+
+        public DeepStorageEventHandler()
+        {
+            _log = this.GetLogger();
+        }
+        
+        public bool IsHandled(string eventName)
+        {
+            // handle all events
+            return true;
+        }
+
+        public void Handle(EventData eventData, string partitionId)
+        {
+            var store = GetEventStore(eventData, partitionId);
+            var bytes = eventData.GetBytes();
+            store.Write(bytes);
+        }
+
+        private static void FlushOverdueStores(object sender, ElapsedEventArgs e)
+        {
+            FlushOverdueStores();
+        }
+
+        public static void FlushOverdueStores()
+        {
+            var overdueStoreKeys = new List<string>();
+            var beforeCount = _eventStores.Count;
+
+            foreach (var store in _eventStores)
+            {
+                if (store.Value.IsFlushOverdue())
+                {
+                    overdueStoreKeys.Add(store.Key);
+                }
+            }
+            foreach (var overdueStoreKey in overdueStoreKeys)
+            {
+                IEventStore overdueStore;
+                if (_eventStores.TryRemove(overdueStoreKey, out overdueStore))
+                {
+                    overdueStore.Flush();
+                    overdueStore.Dispose();
+                }
+            }
+        }
+
+        private IEventStore GetEventStore(EventData eventData, string partitionId)
+        {
+            var receivedAt = eventData.GetReceivedAtHour();
+            var key = string.Format("{0}p{1}", receivedAt, partitionId);
+            if (!_eventStores.ContainsKey(key))
+            {
+                var store = Container.Instance.Resolve<IEventStore>("1");
+                store.Initialize(partitionId, receivedAt);
+                _eventStores[key] = store;
+            }
+
+            return _eventStores[key];
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                FlushOverdueStores();
+            }
+        }
+    }
+}
---
+++ Telemetry/Telemetry.EventProcessor.DeepStorage.WorkerRole/EventStores/BlobStorageEventStore.cs
@@ -0,0 +1,108 @@
+﻿namespace Telemetry.EventProcessor.DeepStorage.Worker.EventStores
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Threading;
+	using Microsoft.WindowsAzure.Storage;
+	using Microsoft.WindowsAzure.Storage.Blob;
+	using Telemetry.Core;
+	using Telemetry.Core.Infrastructure.BlobStorage;
+
+	public class BlobStorageEventStore : EventStoreBase
+	{
+		private string _blobPath;
+		private CloudBlockBlob _blob;
+		private readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
+		private readonly List<string> _blockIds = new List<string>();
+
+		public override int Level { get { return 3; } }
+
+		public override void Initialize(string partitionId, string receivedAtHour)
+		{
+			base.Initialize(partitionId, receivedAtHour);
+			_blobPath = string.Format("{0}/{1}/{2}/p{3}/{4}.json.gz",
+				receivedAtHour.Substring(0, 4),
+				receivedAtHour.Substring(0, 2),
+				receivedAtHour.Substring(6, 2),
+				partitionId, receivedAtHour);
+			CreateBlob();
+		}
+
+		public override void Write(byte[] value)
+		{
+			try
+			{
+				_lock.Wait();
+				using (var lease = new RenewingBlobLease(_blob))
+				{
+					var offset = 0;
+					while (offset < value.Length)
+					{
+						var blockId = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+						var remaining = value.Length - offset;
+						var length = remaining < MaxBufferSize ? remaining : MaxBufferSize;
+						using (var stream = new MemoryStream(value, offset, length))
+						{
+							_blob.PutBlock(blockId, stream, null, AccessCondition.GenerateLeaseCondition(lease.Id));
+							offset += (int) stream.Length;
+						}
+						_blockIds.Add(blockId);
+					}
+					Flush(lease);
+				}
+			}
+			finally
+			{
+				_lock.Release();
+			}
+
+			AfterWrite();
+		}
+
+		private void Flush(RenewingBlobLease lease)
+		{
+			if (_blockIds.Any())
+			{
+				var blockList = _blob.DownloadBlockList(BlockListingFilter.Committed,
+					AccessCondition.GenerateLeaseCondition(lease.Id));
+				var blockIds = blockList.Select(x => x.Name).ToList();
+				_blockIds.AddRange(blockIds);
+				_blob.PutBlockList(blockIds, AccessCondition.GenerateLeaseCondition(lease.Id));
+				_blockIds.Clear();
+			}
+		}
+
+		public override void Flush()
+		{
+			try
+			{
+				_lock.Wait();
+				using (var lease = new RenewingBlobLease(_blob))
+				{
+					Flush(lease);
+				}
+			}
+			finally
+			{
+				_lock.Release();
+			}
+
+			AfterFlush();
+		}
+
+		private void CreateBlob()
+		{
+			var account = CloudStorageAccount.Parse(Config.Get("DeepStorage.OutputConnectionString"));
+			var blobClient = account.CreateCloudBlobClient();
+			var container = blobClient.GetContainerReference(Config.Get("DeepStorage.OutputContainerName"));
+			container.CreateIfNotExists();
+			_blob = container.GetBlockBlobReference(_blobPath);
+			if (!_blob.Exists())
+			{
+				_blob.UploadFromByteArray(new byte[] {},0, 0);
+			}
+		}
+	}
+}
